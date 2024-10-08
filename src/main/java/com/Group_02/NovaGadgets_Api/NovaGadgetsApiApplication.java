48d362df80efb63430c4dda41fe05e8dd5717d9e
Ray <ray_punkeki@hotmail.com>
2024-10-08 01:25:50 -0500

    feat: project structure added

---
+++ src/main/java/com/Group_02/NovaGadgets_Api/NovaGadgetsApiApplication.java
@@ -0,0 +1,13 @@
+package com.Group_02.NovaGadgets_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaGadgetsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaGadgetsApiApplication.class, args);
+	}
+
+}
